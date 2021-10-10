f93a0fdebc558a868b1ef42e7837319b8fcfa764
Ler Tor <0079036184882@mail.ru>
2021-10-10 19:00:52 +0300

    Initial

---
+++ src/users/users.controller.ts
@@ -0,0 +1,33 @@
+import {Body, Controller, Post, Get, UseGuards} from '@nestjs/common';
+import {CreateUserDto} from "./dto/create-user.dto";
+import {UsersService} from "./users.service";
+import {ApiOperation, ApiResponse, ApiTags} from "@nestjs/swagger";
+import {User} from "./user.model";
+import {JwtAuthGuard} from "../auth/jwt-auth.guard";
+import {Roles} from "../auth/roles-auth.decorator";
+import {RolesGuard} from "../auth/roles.guard";
+
+@ApiTags('Пользователи')
+@Controller('users')
+export class UsersController {
+
+    constructor(private userService: UsersService) {
+    }
+
+    @ApiOperation({summary: 'Создание пользователя'})
+    @ApiResponse({status: 200, type: User})
+    @Post()
+    create(@Body() userDto: CreateUserDto){
+        return this.userService.createUser(userDto);
+    }
+
+    @ApiOperation({summary: 'Получение всех пользователей'})
+    @ApiResponse({status: 200, type: [User]})
+    @Roles("ADMIN")
+    @UseGuards(RolesGuard)
+    @Get()
+    getAll(){
+        return this.userService.getAllUsers();
+    }
+
+}
